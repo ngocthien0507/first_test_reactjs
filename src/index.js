9f9d48a486fe1389a42876919f144a18bf9062c2
unknown <ngocthien.contact@gmail.com>
2020-08-19 23:32:07 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const Square = (props) => {
+  return (
+    <button className="square" onClick={() => props.onClickBut(props.id)}>
+      {props.array.map(item => {
+        if (item.id === props.id) {
+          return item.char
+        }
+        else return ""
+      })}
+    </button>
+  );
+}
+
+function renderSquare(i, char, array, onClickBut) {
+  return <Square id={i} char={char} array={array} onClickBut={onClickBut} />;
+}
+
+const Board = () => {
+  const [state, setState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { array: [], char: "X", status: true }
+  )
+  const { array, char, status } = state
+
+  function onClickBut(id) {
+    const dup = array.find(item => item.id === id);
+    if (dup === undefined) {
+      let a = { id: id, char: char }
+      let newarray = array;
+      newarray.push(a);
+      setState({ array: newarray, char: char === "O" ? "X" : "O", status: !status })
+    }
+  }
+
+  return (
+    <div>
+      <div className="status">{status ? 'Next player: X' : 'Next player: O'}</div>
+      <div className="board-row">
+        {renderSquare(0, char, array, onClickBut)}
+        {renderSquare(1, char, array, onClickBut)}
+        {renderSquare(2, char, array, onClickBut)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3, char, array, onClickBut)}
+        {renderSquare(4, char, array, onClickBut)}
+        {renderSquare(5, char, array, onClickBut)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6, char, array, onClickBut)}
+        {renderSquare(7, char, array, onClickBut)}
+        {renderSquare(8, char, array, onClickBut)}
+      </div>
+    </div>
+  );
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
